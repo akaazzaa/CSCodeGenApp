3f339d1b23be58b24ae0a883d31a98ac29d1fa0d
Danny H <145788699+akaazzaa@users.noreply.github.com>
2025-03-07 21:53:04 +0100

    a

---
+++ CSCodeGen.DataAccess/Interface/ITemplateRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSCodeGen.DataAccess.Interface
+{
+    public interface ITemplateRepository
+    {
+
+    }
+}
